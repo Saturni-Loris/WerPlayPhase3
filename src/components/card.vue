28b835088eb3adb16033ece90674ad71f610df6c
Saturni-Loris <loris.saturni@edu.univ-fcomte.fr>
2022-05-20 14:38:16 +0200

    a

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,38 +1,67 @@
 <template>
+  <figure class="flex flex-col">
+    <div
+      class="
+        flex
+        w-full
+        grid-cols-1
+        flex-col
+        gap-6
+        sm:grid-cols-2
+        lg:grid-cols-3
+        xl:grid-cols-4
+      "
+    >
+      <div class="relative cursor-pointer overflow-hidden rounded-lg shadow-lg">
+        <img
+          class="w-full object-cover"
+          src="../assets/basket_photo.png"
+          alt="Flower and sky"
+        />
 
+        <div class="absolute bottom-0 right-0 px-6 py-4">
+          <h4
+            class="
+              text-dark
+              bg-white
+              mb-3
+              flex
+              rounded-lg
+              p-1
+              text-xl
+              font-semibold
+              tracking-tight
+            "
+          >
+            <etoilevide />
 
+            <etoile />
+            <etoile />
+            <etoile />
+          </h4>
+        </div>
+      </div>
+    </div>
 
-
-
-  <figure class="flex flex-col ">
-    <div class="flex flex-col  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
-  <div class="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
-	<img class="object-cover w-full " src="../assets/basket_photo.png" alt="Flower and sky"/>
-
-	<div class="absolute bottom-0 right-0 px-6 py-4">
-	  <h4 class="flex mb-3 text-xl font-semibold tracking-tight text-dark bg-white rounded-lg p-1">
-      <etoilevide />
-
-          <etoile />
-          <etoile />
-          <etoile />
-    </h4>
-	</div>
-</div>
-</div>
-    
-    <figcaption class="relative rounded-b- border-x-2 border-b-2 border-griswerplay px-4 py-8 ">
+    <figcaption
+      class="
+        rounded-b-
+        relative
+        border-x-2 border-b-2 border-griswerplay
+        px-4
+        py-8
+      "
+    >
       <div class="relative flex flex-col">
         <div class="order-first flex items-center pb-4">
-          <h3 class="pr-8 text-2xl font-bold leading-8 text-rougewerplay ">
+          <h3 class="pr-8 text-2xl font-bold leading-8 text-rougewerplay">
             Montbéliard, 25200
           </h3>
-          
         </div>
-        <h3 class="text-m mr-1 font-semibold leading-8 text-gray-400 underline">
+        <h3 class="text-m text-gray-400 mr-1 font-semibold leading-8 underline">
           S’y rendre
         </h3>
-        <h3 class="text-base leading-8 text-gray-400">Rue du Mont Bart</h3>
+        <h3 class="text-gray-400 text-base leading-8">Rue du Mont Bart</h3>
         <hr class="my-2 border-griswerplay" />
         <div class="flex justify-between text-sm font-normal">
           <div><football class="inline-block align-top" /> Football</div>
@@ -44,10 +73,6 @@
       </div>
     </figcaption>
   </figure>
-
-  <svg class="w-6 h-6 mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
-        </svg>
 </template>
 
 
--- src/components/formulaire.vue
+++ src/components/formulaire.vue
@@ -1,75 +1,191 @@
 <template>
-<!-- component -->
-<form class="items-center justify-center p-8">
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-        Nom*
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-orangewerplay rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Arthur">
-      <p class="text-xs italic">*champs obligatoire</p>
-    </div>
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-        Prénom*
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-orangewerplay rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Saturni">
-      <p class="text-xs italic">*champs obligatoire</p>
-    </div>
-  </div>
+  <!-- component -->
+  <form class="items-center justify-center p-8">
+    <div class="-mx-3 mb-6 flex flex-wrap">
+      <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+        <label
+          class="
+            text-gray-700
+            mb-2
+            block
+            text-xs
+            font-bold
+            uppercase
+            tracking-wide
+          "
+          for="grid-first-name"
+        >
+          Nom*
+        </label>
+        <input
+          class="
+            bg-gray-200
+            text-gray-700
+            focus:bg-white
+            mb-3
+            block
+            w-full
+            appearance-none
+            rounded
+            border-2 border-orangewerplay
+            py-3
+            px-4
+            leading-tight
+            focus:outline-none
+          "
+          id="grid-first-name"
+          type="text"
+          placeholder="Arthur"
+        />
+        <p class="text-xs italic">*champs obligatoire</p>
+      </div>
+      <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+        <label
+          class="
+            text-gray-700
+            mb-2
+            block
+            text-xs
+            font-bold
+            uppercase
+            tracking-wide
+          "
+          for="grid-first-name"
+        >
+          Prénom*
+        </label>
+        <input
+          class="
+            bg-gray-200
+            text-gray-700
+            focus:bg-white
+            mb-3
+            block
+            w-full
+            appearance-none
+            rounded
+            border-2 border-orangewerplay
+            py-3
+            px-4
+            leading-tight
+            focus:outline-none
+          "
+          id="grid-first-name"
+          type="text"
+          placeholder="Saturni"
+        />
+        <p class="text-xs italic">*champs obligatoire</p>
+      </div>
 
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-        E-mail*
-      </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border-2 border-orangewerplay rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="arthursaturni@gmail.com">
-      <p class="text-xs italic">*champs obligatoire</p>
+      <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+        <label
+          class="
+            text-gray-700
+            mb-2
+            block
+            text-xs
+            font-bold
+            uppercase
+            tracking-wide
+          "
+          for="grid-first-name"
+        >
+          E-mail*
+        </label>
+        <input
+          class="
+            bg-gray-200
+            text-gray-700
+            focus:bg-white
+            mb-3
+            block
+            w-full
+            appearance-none
+            rounded
+            border-2 border-orangewerplay
+            py-3
+            px-4
+            leading-tight
+            focus:outline-none
+          "
+          id="grid-first-name"
+          type="text"
+          placeholder="arthursatuni@gmail.com"
+        />
+        <p class="text-xs italic">*champs obligatoire</p>
+      </div>
     </div>
-  </div>
-  
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-        Votre message
-      </label>
-      <textarea class=" no-resize appearance-none block w-full text-gray-700 border-2 border-orangewerplay rounded py-3 px-4 mb-3 leading-tight  h-48 resize-none" id="message"></textarea>
-    </div>
-  </div>
-  
-  <div class="md:flex md:items-center">
-    <div class="">
-      <button class="  bg-orangewerplay  text-white font-bold py-2 px-16 rounded" type="button">
-        Envoyer
-      </button>
-    </div>
-    <div class="md:w-2/3"></div>
-  </div>
 
-  <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-            <div class="flex flex-col ">
-            <div class="flex flex-col py-6">
-                    <label class="mt-1 inline-flex text-xs text-left  ">
-                    <input type="checkbox" class="form-checkbox h-5 w-5 " checked /><span
-                        class="ml-2 "
-                        >J’ai compris et j’accepte (1)</span
-                    >
-                    </label>
-            </div>
-            </div>
+    <div class="-mx-3 mb-6 flex flex-wrap">
+      <div class="w-full px-3">
+        <label
+          class="
+            text-gray-700
+            mb-2
+            block
+            text-xs
+            font-bold
+            uppercase
+            tracking-wide
+          "
+        >
+          Votre message
+        </label>
+        <textarea
+          class="
+            no-resize
+            text-gray-700
+            mb-3
+            block
+            h-48
+            w-full
+            resize-none
+            appearance-none
+            rounded
+            border-2 border-orangewerplay
+            py-3
+            px-4
+            leading-tight
+          "
+          id="message"
+        ></textarea>
+      </div>
+    </div>
+
+    <div class="md:flex md:items-center">
+      <div class="">
+        <button
+          class="text-white rounded bg-orangewerplay py-2 px-16 font-bold"
+          type="button"
+        >
+          Envoyer
+        </button>
+      </div>
+      <div class="md:w-2/3"></div>
+    </div>
+
+    <div class="-mx-3 mb-6 flex flex-wrap">
+      <div class="w-full px-3">
+        <div class="flex flex-col">
+          <div class="flex flex-col py-6">
+            <label class="mt-1 inline-flex text-left text-xs">
+              <input
+                type="checkbox"
+                class="form-checkbox h-5 w-5"
+                checked
+              /><span class="ml-2">J’ai compris et j’accepte (1)</span>
+            </label>
+          </div>
         </div>
-      
-      <p class="text-xs italic">(1) En communiquant mes informations, je consens à être recontacté par voie électronique par WerPlay. Ces données seront conservées durant 30 jours afin de pouvoir traiter au mieux ma demande. Je pourrai faire modifier ou supprimer mes informations sur simple demande. En savoir plus.</p>
-</div>
-  
-</form>
+      </div>
 
-
-
-
-
-
-
-
+      <p class="text-xs italic">
+        (1) En communiquant mes informations, je consens à être recontacté par
+        voie électronique par WerPlay. Ces données seront conservées durant 30
+        jours afin de pouvoir traiter au mieux ma demande. Je pourrai faire
+        modifier ou supprimer mes informations sur simple demande. En savoir
+        plus.
+      </p>
+    </div>
+  </form>
 </template>
